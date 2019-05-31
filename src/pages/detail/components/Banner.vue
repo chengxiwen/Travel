ded166d8f682dcb514cffe53013c3f0d84297ec1
Cindy-Cheng <chengxiwenhnu@163.com>
2019-05-31 17:02:09 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1810/8e/8ec325e3d27cf637a3.img.jpg_600x330_7d33087f.jpg" />
+      <div class="banner-info">
+        <div class="banner-title">
+          花田酒溪中国黄酒民俗文化村
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe623;</span>
+          39
+        </div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/201312/16/3ea492bf4dbf4f014191733ab6918ec4.jpg_600x330_bf521ad3.jpg',
+        'http://img1.qunarzz.com/sight/p0/1702/2c/2cf38c20b40d38e5a3.water.jpg_600x330_b8bfb8a2.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display: flex
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      line-height: .6rem
+      color: #fff
+      background-image: linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+      .banner-title
+        flex: 1
+        font-size: .32rem
+        padding: 0 .2rem
+      .banner-number
+        height: .32rem
+        line-height: .32rem
+        margin-top: .14rem
+        padding: 0 .4rem
+        border-radius: .2rem
+        background: rgba(0, 0, 0, .8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
